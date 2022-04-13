c452e82a2a8d53f776f6ce67c326b6b8ad55430d
dimka77783 <dimka77783@mai.ru>
2022-04-13 22:36:30 +0300

    def ok

--- 2TS_bez_EE_f.py
+++ 2TS_bez_EE_f.py
@@ -62,6 +62,10 @@ def find_element_xpaths(n):
 def find_element_xpath(n):
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, n))).click()
 
+def find_element_xpath_DOWN(n):
+    driver.find_element(By.XPATH,n).send_keys(Keys.DOWN)
+    time.sleep(3)
+
 def find_element_xpath_prot(n, path_protokol):
     driver.find_element(By.XPATH, n).send_keys(path_protokol)
 
@@ -76,7 +80,7 @@ def send_prot(s,n):
     input_n_prot = driver.find_element(By.XPATH, s)
     input_n_prot.clear()
     input_n_prot.send_keys(n)
-    time.sleep(2)
+    time.sleep(3)
 
 def send_date(n, day_count):
     driver.find_element(By.XPATH, n).clear()
@@ -88,21 +92,6 @@ def send_date2(n, day_count):
     driver.find_element(By.XPATH, n).send_keys(Keys.LEFT * int(day_count))
     time.sleep(2)
 
-def link_lists():
-
-    my_ul2 = driver.find_elements(By.XPATH, "//ul[@class='b-page']")  # блок со страницам
-    if len(my_ul2)==1:
-        my_ul = driver.find_element(By.XPATH, "//ul[@class='b-page']")  # блок со страницам
-        time.sleep(7)
-        all_li = my_ul.find_elements(By.TAG_NAME, "li")  # оперделяем кол-во страниц
-        time.sleep(7)
-        global link_list
-        link_list = []
-        for li in all_li:
-            y = li.text
-            link_list.append(y)
-
-    return link_list
 try:
     driver.get(url=url)
     time.sleep(15)
@@ -113,37 +102,89 @@ try:
     print('все')
     find_element_link("2ТС без ТЭ")
     print('2ТС ТЭ')
-    print(type(link_lists()))
-    rec = 1
-    while rec == 1:
+
+    my_ul2 = driver.find_elements(By.XPATH, "//ul[@class='b-page']")  # блок со страницам
+    if len(my_ul2) == 1:
+        my_ul = driver.find_element(By.XPATH, "//ul[@class='b-page']")  # блок со страницам
+        time.sleep(7)
+        all_li = my_ul.find_elements(By.TAG_NAME, "li")  # оперделяем кол-во страниц
+        time.sleep(7)
+        link_list = []
+        for li in all_li:
+            y = li.text
+            link_list.append(y)
+        rec = 1
+        while rec == 1:
+            for i in range(2, m_row + 1):
+                cell_obj = sheet_obj.cell(row=i, column=1)  # В column= подставляем номер нужной колонки
+                number_ideas = cell_obj.value
+                cell_ob = sheet_obj.cell(row=i, column=10)  # В column= подставляем номер нужной колонки
+                name_ideas = cell_ob.value
+                for y in link_list:
+                    ds = driver.find_elements(By.XPATH, " // *[ @ href = 'Offer.aspx?id=" + str(number_ideas) + "']")
+                    time.sleep(5)
+
+                    if len(ds) >= 1:
+                        find_element_xpath(" // *[ @ href = 'Offer.aspx?id=" + str(number_ideas) + "']")
+                        find_id("ctl00_ctl00_ctl00_MainContent_VMenuRightContent_OfferMenu_linkEngine83")
+                        find_id("ctl00_ctl00_ctl00_MainContent_CommandsMenu_linkEdit")
+                        driver.switch_to.frame(driver.find_element(By.TAG_NAME, "iframe"))
+                        time.sleep(5)
+                        send_prot("// input[@class='alpaca-control form-control' and @id = 'alpaca2']", n)
+                        send_date("// input[@class='alpaca-control form-control' and @id = 'alpaca3']", day_count)
+                        send_date2("// input[@class='alpaca-control form-control' and @id = 'alpaca4']", day_count)
+                        driver.switch_to.default_content()
+                        time.sleep(3)
+                        find_id("ctl00_ctl00_ctl00_MainContent_CommandsMenu_linkSave")
+                        find_id("ctl00_ctl00_ctl00_MainContent_VMenuRightContent_"
+                                                "OfferMenu_linkDocuments")
+                        find_element_xpath_prot("//input[@type='file'][contains(@id,'FileUpload1')]", path_protokol)
+                        find_element_xpath("//input[@value='Прикрепить файл']")
+                        """
+                        find_id("ctl00_ctl00_ctl00_MainContent_VMenuRightContent_OfferMenu_linkStatus")
+                        find_id("ctl00_ctl00_ctl00_MainContent_CommandsMenu_linkEdit")
+                        find_element_xpath_DOWN("(//select[@class='ddlStatusList'])[2]")
+                        find_element_xpath("//input[@value='Изменить статус']")
+                        """
+                        find_element_link("Все идеи")
+                        print(name_ideas)
+                        break
+                    else:
+                        rec = 0
+                        w = int(y) + 1
+                        print(f"переходим на страницу: {w}")
+                        driver.find_element(By.LINK_TEXT,""+str(w)+"").click()
+                        time.sleep(3)
+
+    else:
         for i in range(2, m_row + 1):
             cell_obj = sheet_obj.cell(row=i, column=1)  # В column= подставляем номер нужной колонки
             number_ideas = cell_obj.value
             cell_ob = sheet_obj.cell(row=i, column=10)  # В column= подставляем номер нужной колонки
             name_ideas = cell_ob.value
-            for y in link_list:
-                ds = driver.find_elements(By.XPATH, " // *[ @ href = 'Offer.aspx?id=" + str(number_ideas) + "']")
-                time.sleep(5)
-
-                if len(str(ds)) >= 1:
-                    find_element_xpath(" // *[ @ href = 'Offer.aspx?id=" + str(number_ideas) + "']")
-                    find_id("ctl00_ctl00_ctl00_MainContent_VMenuRightContent_OfferMenu_linkEngine83")
-                    find_id("ctl00_ctl00_ctl00_MainContent_CommandsMenu_linkEdit")
-                    driver.switch_to.frame(driver.find_element(By.TAG_NAME, "iframe"))
-                    time.sleep(5)
-                    send_prot("// input[@class='alpaca-control form-control' and @id = 'alpaca2']", n)
-                    print(int(day_count))
-                    send_date("// input[@class='alpaca-control form-control' and @id = 'alpaca3']", day_count)
-                    send_date2("// input[@class='alpaca-control form-control' and @id = 'alpaca4']", day_count)
-                    driver.switch_to.default_content()
-                    time.sleep(3)
-                    find_id("ctl00_ctl00_ctl00_MainContent_CommandsMenu_linkSave")
-                    find_id("ctl00_ctl00_ctl00_MainContent_VMenuRightContent_"
-                                            "OfferMenu_linkDocuments")
-                    time.sleep(3)
-                    find_element_xpath_prot("//input[@type='file'][contains(@id,'FileUpload1')]", path_protokol)
-                    find_id("ctl00_ctl00_ctl00_MainContent_VMenuLeftConte_OfferContent_AttachedFilesCore_BtnSave")
-
+            find_element_xpath(" // *[ @ href = 'Offer.aspx?id=" + str(number_ideas) + "']")
+            find_id("ctl00_ctl00_ctl00_MainContent_VMenuRightContent_OfferMenu_linkEngine83")
+            find_id("ctl00_ctl00_ctl00_MainContent_CommandsMenu_linkEdit")
+            driver.switch_to.frame(driver.find_element(By.TAG_NAME, "iframe"))
+            time.sleep(5)
+            send_prot("// input[@class='alpaca-control form-control' and @id = 'alpaca2']", n)
+            send_date("// input[@class='alpaca-control form-control' and @id = 'alpaca3']", day_count)
+            send_date2("// input[@class='alpaca-control form-control' and @id = 'alpaca4']", day_count)
+            driver.switch_to.default_content()
+            time.sleep(3)
+            find_id("ctl00_ctl00_ctl00_MainContent_CommandsMenu_linkSave")
+            find_id("ctl00_ctl00_ctl00_MainContent_VMenuRightContent_"
+                                    "OfferMenu_linkDocuments")
+            find_element_xpath_prot("//input[@type='file'][contains(@id,'FileUpload1')]", path_protokol)
+            find_element_xpath("//input[@value='Прикрепить файл']")
+            """
+            find_id("ctl00_ctl00_ctl00_MainContent_VMenuRightContent_OfferMenu_linkStatus")
+            find_id("ctl00_ctl00_ctl00_MainContent_CommandsMenu_linkEdit")
+            find_element_xpath_DOWN("(//select[@class='ddlStatusList'])[2]")
+            find_element_xpath("//input[@value='Изменить статус']")
+            """
+            find_element_link("Все идеи")
+            print(name_ideas)
 
 except Exception as ex:
     print(ex)
